480edabbd9d98f540d70deb0d9720cb342f66f8a
Zan-Dev <a.fauzan.idn@gmail.com>
2024-11-26 11:29:09 +0700

    add error handling

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
+use App\Http\Resources\TodolistResource;
 use App\Models\Todolist;
+use Dotenv\Exception\ValidationException;
+use Exception;
 use Illuminate\Http\Request;
 
 class TodolistController extends Controller
@@ -14,7 +18,8 @@ class TodolistController extends Controller
     {
         $todolist = Todolist::latest()->get();
 
-        return response()->json([$todolist], 200);
+        // return response()->json([$todolist], 200);
+        return TodolistResource::collection($todolist);
     }
 
     /**
@@ -22,14 +27,34 @@ class TodolistController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'title' => 'required|string',
-            'desc' => 'required|string',
-            'status' => 'required|boolean',
-        ]);
+        try {
+            $data = $request->validate([
+                'title' => 'required|string',
+                'desc' => 'required|string',
+                'status' => 'required|boolean|in:0,1',
+            ]);
+    
+            $todolist = Todolist::create($data);
 
-        $todolist = Todolist::create($data);
-        return response()->json($todolist, 200);
+            return response()->json([
+                'message' => 'Successfully add data',
+                'data' => new TodolistResource($todolist),
+            ], 201);
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'data validation fail',
+                'errors' => $e->getMessage()
+            ], 422);
+
+        } catch (Exception $e){
+            Log::error("error when saving data". $e->getMessage());
+
+            return response()->json([
+                'message' => 'error when saving data',
+                'errors' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -38,7 +63,7 @@ class TodolistController extends Controller
     public function show(string $id)
     {
         $todolist = Todolist::find($id);
-        if (!$todolist) return response()->json(['message' => 'Todolist Not Found', 404]);
+        if (!$todolist) return response()->json(['message' => 'Todolist Not Found'], 404);
         return response()->json($todolist);
     }
 
@@ -46,11 +71,40 @@ class TodolistController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-    {
-        $todolist = Todolist::find($id);
-        if (!$todolist) return response()->json(['message' => 'Todolist Not Found', 404]);
-        $todolist->update($request->all());
-        return response()->json($todolist);
+    {                          
+        try {
+            $data = $request->validate([
+                'title' => 'required|string',
+                'desc' => 'required|string',
+                'status' => 'required|boolean|in:0,1',
+            ]);
+    
+            $todolist = Todolist::find($id);
+
+            if (!$todolist) return response()->json(['message' => 'Todolist Not Found'],404);
+
+            $todolist->update($data);
+
+            return response()->json([
+                'status' => 'updated',
+                'message' => 'Successfully update data',
+                'data' => new TodolistResource($todolist),
+            ], 200);
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'data validation fail',
+                'errors' => $e->getMessage()
+            ], 422);
+            
+        } catch (Exception $e){
+            Log::error("error when update data". $e->getMessage());
+
+            return response()->json([
+                'message' => 'error when update data',
+                'errors' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -59,8 +113,11 @@ class TodolistController extends Controller
     public function destroy(string $id)
     {
         $todolist = Todolist::find($id);
-        if (!$todolist) return response()->json(['message' => 'Todolist Not Found', 404]);
+        if (!$todolist) return response()->json([           
+            'message' => 'Todolist Not Found'], 404);
         $todolist->delete();
-        return response()->json($todolist);
+        return response()->json([ 
+            'status' => 'deleted',
+            'data' => $todolist], 200);
     }
 }

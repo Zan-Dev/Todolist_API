58cdd577b7ce27e19ac3e6b4e4044dc1c26ba0ca
Zan-Dev <a.fauzan.idn@gmail.com>
2024-11-27 19:29:19 +0700

    add API CRUD user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Exception;
 use Illuminate\Http\Request;
-
+use Illuminate\Validation\ValidationException;
+use PhpParser\Node\Stmt\Catch_;
 
 class UserController extends Controller
 {
@@ -23,10 +26,37 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'username' => 'required|min:5',
-            'email' => 'required|email',            
-        ]);
+        try {
+            $data = $request->validate([
+                'username' => 'required|min:5',
+                'email' => 'required|email',         
+                'password' => 'required|confirmed|min:8',
+            ]);
+
+            $data['password'] = Hash::make($request->password);
+    
+            User::create($data);            
+            
+            return response()->json([
+                'message' => 'successfully create a user',
+                'data' => $data
+            ], 201);
+
+        } catch (ValidationException $e) {
+            
+            return response()->json([
+                'message' => 'data validation fail',
+                'error' => $e->getMessage(),
+            ], 422);
+
+        } catch (Exception $e){
+
+            return response()->json([
+                'message' => 'error when saving data',
+                'error' => $e->getMessage(),
+            ], 500);
+
+        }
     }
 
     /**
@@ -34,15 +64,66 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = User::find($id);
+        
+        if(!$user){
+            return response()->json([
+                'message' => 'data not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'data found',
+            'data' => $user
+        ], 200);
+
     }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-    {
-        //
+    {       
+
+        try {
+            $data = $request->validate([
+                'username' => 'required|min:5',
+                'email' => 'required|email',    
+                'password' => 'nullable|confirmed|min:8',
+            ]);
+
+            $user = User::find($id);
+            
+            if(!$user){
+                return response()->json([
+                    'message' => 'data not found'
+                ], 404);
+            }
+
+            if($request->has('password')){$data['password'] = Hash::make($request->password);}
+            else{unset($data['password']);}
+
+            $user->update($data);
+
+            return response()->json([
+                'message' => 'data successfully modified',
+                'data' => $data
+            ], 200);
+            
+
+        } catch (ValidationException $e) {
+            
+            return response()->json([
+                'message' => 'data validation fail',
+                'error' => $e->getMessage()
+            ],422);
+        } catch (Exception $e){
+            
+            return response()->json([
+                'message' => 'error when update data',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -50,6 +131,19 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::find($id);
+        
+        if(!$user){
+            return response()->json([
+                'message' => 'data not found'
+            ], 404);
+        }        
+
+        $user->delete();
+
+        return response()->json([
+            'message' => 'data successfully deleted',
+            'data' => $user
+        ], 200);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $primaryKey = 'id_user';
    
     protected $fillable = [
+        'username',
         'name',
         'email',
         'password',
